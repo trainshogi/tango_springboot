b0ce811b0d717e60dcae083e2f5b6dbd0f82360a
trainshogi <densyashogi@gmail.com>
2021-07-28 21:53:22 +0900

    initial commit

---
+++ src/main/java/com/nkkuma/tango_springboot/TangoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nkkuma.tango_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TangoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TangoSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nkkuma/tango_springboot/TangoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkkuma.tango_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TangoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
